00ac9aaf8cad4575b11cd65ce70b1c6173ad367b
danisimore <danisimore@yandex.ru>
2022-11-20 18:36:53 +0300

    Third commit (fix)

--- landingwebsite_root/landingwebsite/settings.py
+++ landingwebsite_root/landingwebsite/settings.py
@@ -133,12 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
-# For dev version
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'landingwebsite/static/'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
